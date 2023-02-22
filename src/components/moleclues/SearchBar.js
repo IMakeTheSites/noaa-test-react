6f29489035dd47cc1c0cf9441b6671cf200aadb8
IMakeTheSites <imakethesites@gmail.com>
2023-02-21 19:45:35 -0500

    Updates for search bar

--- src/components/moleclues/SearchBar.js
+++ src/components/moleclues/SearchBar.js
@@ -1,5 +1,26 @@
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+
 const SearchBar = () => {
-  return <div>Search Bar</div>;
+  return (
+    <div>
+      <OutlinedInput
+        id="outlined-adornment-password"
+        type="text"
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton aria-label="toggle password visibility" edge="end">
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        }
+        label="Password"
+        placeholder="Search Pokemon"
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
